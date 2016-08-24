ada07d128b3adc46a4bf406fe3ddc918ee3283b4
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-08-24 15:59:02 -0300

    Updating project version

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject br.eng.crisjr.failproof "0.1.0"
+(defproject br.eng.crisjr.failproof "0.1.1"
     :description "API for Failproof Checklists"
     :url "http://crisjr.eng.br"
     :license {:name "Beerware 42"
-            :url "http://people.freebsd.org/~phk"}
+              :url "http://people.freebsd.org/~phk"}
     :dependencies [[org.clojure/clojure "1.7.0"]]
     :main ^:skip-aot br.eng.crisjr.failproof.web
     :target-path "target/%s"
