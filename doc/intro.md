ff52355aa96e81ee657d65912558f461398939ed
Cris Silva Jr <ishiikurisu@yahoo.com>
2020-07-01 23:54:25 -0300

    Deleting everything

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to br.eng.crisjr.web
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject br.eng.crisjr.failproof "0.2.4"
-    :description "API for Failproof Checklists"
-    :url "http://wwww.crisjr.eng.br"
-    :license {:name "Beerware 42"
-              :url "http://people.freebsd.org/~phk"}
-    :dependencies [[org.clojure/clojure "1.7.0"]]
-    :main ^:skip-aot br.eng.crisjr.failproof.tools
-    :target-path "target/%s"
-    :profiles {:uberjar {:aot :all}})
