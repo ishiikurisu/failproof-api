1e8579cb0b455bfc39ff22c0d8c8b3a5fb2a0769
Cris Silva Jr <ishiikurisu@yahoo.com>
2020-07-14 21:58:16 -0300

    Implement route to create users

---
+++ test_routes.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+require 'rack/test'
+require 'test/unit'
+require './main'
+
+class MainRoutesTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+  def app
+    Sinatra::Application
+  end
+
+  def test_post_create_users
+    $db.drop
+    $db.setup
+    
+    data = {
+      "username": "user",
+      "password": "password",
+      "notes": nil,
+    }
+    post '/users/create', data.to_json, "CONTENT_TYPE" => "application/json"
+    assert last_response.ok?
+    
+    expected_auth_key = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMSJ9.OiHSk5Cl2k7cxj2yhsNuEHtjBdAc3PbJk3X6rl4ROZ0"
+    result_auth_key = JSON.parse(last_response.body)["auth_key"]
+    assert expected_auth_key == result_auth_key
+  end
+end
