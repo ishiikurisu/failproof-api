2d56b10cd635db48e4044ee447cfb808495eb201
Cris Jr <crisjr@pm.me>
2020-07-20 10:38:42 -0300

    Authorize users

--- db/db.rb
+++ db/db.rb
@@ -4,7 +4,7 @@ require 'jwt'
 class Database
   attr_reader :create_tables_sql
 
-  def initialize dboptions
+  def initialize dboptions, sql_folder
     host = dboptions['host']
     port = dboptions['port']
     dbname = dboptions['dbname']
@@ -12,12 +12,12 @@ class Database
     password = dboptions['password']
     tty = nil
     options = nil
-
-    create_tables_sql = File.open("./db/sql/create_tables.sql", "r") do |file|
-      @create_tables_sql = file.read
-    end
-    create_user_sql = File.open("./db/sql/create_user.sql", "r") do |file|
-      @create_user_sql = file.read
+    
+    @sql = {}
+    Dir["#{sql_folder}/*.sql"].each do |fn|
+      File.open(fn) do |fp|
+        @sql[fn.split("/")[-1].chomp(".sql")] = fp.read
+      end
     end
 
     @conn = PG.connect(host, port, options, tty, dbname, user, password)
@@ -25,7 +25,7 @@ class Database
   end
 
   def create_user username, password, notes
-    create_user_sql = @create_user_sql % {
+    create_user_sql = @sql['create_user'] % {
       :username => username,
       :password => password,
       :notes => (notes == nil)? "" : notes,
@@ -35,8 +35,29 @@ class Database
     result.each_row do |row|
       user_id = row[0]
     end
-    payload = {:user_id => user_id}
-    JWT.encode payload, 'random_secret', 'HS256'
+    auth_key = {:user_id => user_id}
+    JWT.encode auth_key, 'random_secret', 'HS256'
+  end
+  
+  def auth_user username, password
+    auth_user_sql = @sql['auth_user'] % {
+      :username => username,
+      :password => password,
+    }
+    
+    user_id = nil
+    notes = nil
+    @conn.exec(auth_user_sql).each_row do |row|
+      user_id = row[0]
+      notes = row[4]
+    end
+    
+    auth_key = (user_id == nil)? nil : JWT.encode({:user_id => user_id}, 'random_secret', 'HS256')
+      
+    return {
+      "auth_key" => auth_key,
+      "notes" => notes,
+    }
   end
   
   def drop
@@ -44,6 +65,6 @@ class Database
   end
   
   def setup
-    @conn.exec @create_tables_sql
+    @conn.exec @sql['create_tables']
   end
 end
--- test_routes.rb
+++ test_routes.rb
@@ -25,4 +25,35 @@ class MainRoutesTest < Test::Unit::TestCase
     result_auth_key = JSON.parse(last_response.body)["auth_key"]
     assert expected_auth_key == result_auth_key
   end
+  
+  def test_post_auth_users
+    # inexistent user
+    $db.drop
+    $db.setup
+    
+    data = {
+      "username": "user",
+      "password": "password",
+    }
+    post '/users/auth', data.to_json, "CONTENT_TYPE" => "application/json"
+    assert last_response.ok?
+    
+    expected_auth_key = nil
+    result_auth_key = JSON.parse(last_response.body)["auth_key"]
+    assert expected_auth_key == result_auth_key
+    
+    # existent user
+    test_post_create_users
+    
+    post '/users/auth', data.to_json, "CONTENT_TYPE" => "application/json"
+    assert last_response.ok?
+    
+    expected_auth_key = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMSJ9.OiHSk5Cl2k7cxj2yhsNuEHtjBdAc3PbJk3X6rl4ROZ0"
+    result_auth_key = JSON.parse(last_response.body)["auth_key"]
+    assert expected_auth_key == result_auth_key
+    
+    expected_notes = ""
+    result_notes = JSON.parse(last_response.body)["notes"]
+    assert expected_notes == result_notes
+  end
 end
