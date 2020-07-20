f6dea200a733123fa36a39d54074fe75f2832478
Cris Jr <crisjr@pm.me>
2020-07-20 15:44:43 -0300

    Add test case for long notes

--- test_routes.rb
+++ test_routes.rb
@@ -65,4 +65,33 @@ class MainRoutesTest < Test::Unit::TestCase
     result_notes = JSON.parse(last_response.body)["notes"]
     assert expected_notes == result_notes
   end
+  
+  def test_create_user_with_notes
+    $db.drop
+    $db.setup
+    
+    expected_notes = %Q(# My first checklist
+This is what I need to do
+- [x] This is a done item
+- [ ] This item is still pending
+)
+    
+    data = {
+      "username": "user",
+      "password": "password",
+      "notes": expected_notes,
+    }
+    post '/users/create', data.to_json, "CONTENT_TYPE" => "application/json"
+    assert last_response.ok?
+    
+    expected_auth_key = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMSJ9.OiHSk5Cl2k7cxj2yhsNuEHtjBdAc3PbJk3X6rl4ROZ0"
+    result_auth_key = JSON.parse(last_response.body)["auth_key"]
+    assert expected_auth_key == result_auth_key
+    
+    post '/users/auth', data.to_json, "CONTENT_TYPE" => "application/json"
+    assert last_response.ok?
+    
+    result_notes = JSON.parse(last_response.body)["notes"]
+    assert expected_notes == result_notes
+  end
 end
