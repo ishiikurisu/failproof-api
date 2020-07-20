bfbab129d3a385330f4cfaf4042eedfbfb5e7426
Cris Jr <crisjr@pm.me>
2020-07-20 10:38:42 -0300

    Authorize users

--- db/sql/create_user.sql
+++ db/sql/create_user.sql
@@ -1,5 +1,5 @@
 INSERT INTO users(username,password,admin,notes)
-SELECT '%{username}','%{password}','off','#{notes}'
+SELECT '%{username}','%{password}','off','%{notes}'
 WHERE NOT EXISTS (
   SELECT * FROM users WHERE username='%{username}')
 RETURNING *;
