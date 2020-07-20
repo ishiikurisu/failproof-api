a8dede5419b72aa0ee3da2c70b873d9d1f92a644
Cris Jr <crisjr@pm.me>
2020-07-20 10:38:42 -0300

    Authorize users

--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ require './db/db.rb'
 File.open('./.config/options.json') do |file|
   $options = JSON.parse file.read
 end
-$db = Database.new $options
+$db = Database.new $options, "./db/sql"
 
 # ROUTES
 get '/' do
@@ -17,4 +17,10 @@ post '/users/create' do
   data = JSON.parse request.body.read
   auth_key = $db.create_user data['username'], data['password'], data['notes']
   return {"auth_key" => auth_key}.to_json
+end
+
+post '/users/auth' do
+  data = JSON.parse request.body.read
+  payload = $db.auth_user data['username'], data['password']
+  return payload.to_json
 end
