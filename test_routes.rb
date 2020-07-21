61852fa36b2ab2bde474fd43d2eb60f57dca8cfc
Cris Silva Jr <ishiikurisu@yahoo.com>
2020-07-21 16:00:27 -0300

    Get notes from user

--- test_routes.rb
+++ test_routes.rb
@@ -13,7 +13,7 @@ class MainRoutesTest < Test::Unit::TestCase
     # inexistent user
     $db.drop
     $db.setup
-    
+
     data = {
       "username": "user",
       "password": "password",
@@ -21,61 +21,61 @@ class MainRoutesTest < Test::Unit::TestCase
     }
     post '/users/create', data.to_json, "CONTENT_TYPE" => "application/json"
     assert last_response.ok?
-    
+
     expected_auth_key = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMSJ9.OiHSk5Cl2k7cxj2yhsNuEHtjBdAc3PbJk3X6rl4ROZ0"
     result_auth_key = JSON.parse(last_response.body)["auth_key"]
     assert expected_auth_key == result_auth_key
-    
+
     # existing user
     post '/users/create', data.to_json, "CONTENT_TYPE" => "application/json"
     assert last_response.ok?
-    
+
     expected_auth_key = nil
     result_auth_key = JSON.parse(last_response.body)["auth_key"]
     assert expected_auth_key == result_auth_key
   end
-  
+
   def test_post_auth_users
     # inexistent user
     $db.drop
     $db.setup
-    
+
     data = {
       "username": "user",
       "password": "password",
     }
     post '/users/auth', data.to_json, "CONTENT_TYPE" => "application/json"
     assert last_response.ok?
-    
+
     expected_auth_key = nil
     result_auth_key = JSON.parse(last_response.body)["auth_key"]
     assert expected_auth_key == result_auth_key
-    
+
     # existent user
     test_post_create_users
-    
+
     post '/users/auth', data.to_json, "CONTENT_TYPE" => "application/json"
     assert last_response.ok?
-    
+
     expected_auth_key = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMSJ9.OiHSk5Cl2k7cxj2yhsNuEHtjBdAc3PbJk3X6rl4ROZ0"
     result_auth_key = JSON.parse(last_response.body)["auth_key"]
     assert expected_auth_key == result_auth_key
-    
+
     expected_notes = ""
     result_notes = JSON.parse(last_response.body)["notes"]
     assert expected_notes == result_notes
   end
-  
+
   def test_create_user_with_notes
     $db.drop
     $db.setup
-    
+
     expected_notes = %Q(# My first checklist
 This is what I need to do
 - [x] This is a done item
 - [ ] This item is still pending
 )
-    
+
     data = {
       "username": "user",
       "password": "password",
@@ -83,21 +83,44 @@ This is what I need to do
     }
     post '/users/create', data.to_json, "CONTENT_TYPE" => "application/json"
     assert last_response.ok?
-    
+
     expected_auth_key = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMSJ9.OiHSk5Cl2k7cxj2yhsNuEHtjBdAc3PbJk3X6rl4ROZ0"
     result_auth_key = JSON.parse(last_response.body)["auth_key"]
     assert expected_auth_key == result_auth_key
-    
+
     post '/users/auth', data.to_json, "CONTENT_TYPE" => "application/json"
     assert last_response.ok?
-    
+
     result_notes = JSON.parse(last_response.body)["notes"]
     assert expected_notes == result_notes
   end
-  
+
   def test_hide_password
     password = "some random string"
     encrypted = $db.hide password
     assert password != encrypted
+  end
+
+  def test_get_checklists
+    # user does not exist
+    $db.drop
+    $db.setup
+    auth_key = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMSJ9.OiHSk5Cl2k7cxj2yhsNuEHtjBdAc3PbJk3X6rl4ROZ0"
+    get "/notes?auth_key=#{auth_key}"
+    assert last_response.ok?
+    result_notes = JSON.parse(last_response.body)["notes"]
+    assert result_notes == nil
+
+    # existing user
+    test_create_user_with_notes
+    expected_notes = %Q(# My first checklist
+This is what I need to do
+- [x] This is a done item
+- [ ] This item is still pending
+)
+    get "/notes?auth_key=#{auth_key}"
+    assert last_response.ok?
+    result_notes = JSON.parse(last_response.body)["notes"]
+    assert expected_notes == result_notes
   end
 end
