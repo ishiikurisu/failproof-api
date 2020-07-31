8cb11dfb779ef8b8f6c89c792e3edb37ba06a575
Cris Silva Jr <ishiikurisu@yahoo.com>
2020-07-31 09:37:59 -0300

    Include auto-update last updated timestamp on users

--- db/sql/create_tables.sql
+++ db/sql/create_tables.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS users (
   username VARCHAR(32) NOT NULL,
   password VARCHAR(256) NOT NULL,
   admin BOOLEAN NOT NULL,
+  last_updated TIMESTAMP DEFAULT current_timestamp
   notes TEXT NOT NULL
 );
