c3209e23bdc78b52d9aa499d3096c6a7c08dfc38
Cris Jr <crisjr@pm.me>
2021-12-09 15:28:25 -0300

    Define basic server

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "http://www.liberdade.bsb.br/util/fpcl"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [compojure "1.6.1"]
+                 [http-kit "2.3.0"]
+                 [ring/ring-defaults "0.3.2"]
+                 [org.clojure/data.json "0.2.6"]
+                 [selmer "1.12.40"]]
   :main ^:skip-aot br.bsb.liberdade.fpcl.api
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/br/bsb/liberdade/fpcl/api.clj
+++ src/br/bsb/liberdade/fpcl/api.clj
@@ -1,7 +1,27 @@
 (ns br.bsb.liberdade.fpcl.api
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.data.json :as json]
+            [org.httpkit.server :as server]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer :all]
+            [selmer.parser :refer :all]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+; ##########
+; # ROUTES #
+; ##########
+(defn index-page [req]
+  {:status 200
+   :headers {"Content-Type" "text/json"}
+   :body (str (json/write-str {:error "not defined yet"}))})
+
+(defroutes app-routes
+  (GET "/" [] index-page))
+
+; ###############
+; # Entry point #
+; ###############
+(defn -main [& args]
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
+    (server/run-server (wrap-defaults #'app-routes site-defaults) {:port port})
+    (println (str "Listening at http://localhost:" port "/"))))
